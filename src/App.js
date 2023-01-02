23cdeaed220e126b54601ac0e14657d2881047fe
Zack Eisbach <zack.eisbach@gmail.com>
2023-01-02 15:05:19 -0500

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import Game from './components/Game'
+import { HashRouter } from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
-            <Game />
+            <HashRouter base="/">
+                <Game />
+            </HashRouter>
         )
     }
 }
